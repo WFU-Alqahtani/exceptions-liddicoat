bce41af54d5228fe4788f4d6761610f20f1f7f04
jackliddicoat <j@liddi.net>
2024-09-03 10:08:49 -0400

    Fixed file to catch exception

--- src/Exception.java
+++ src/Exception.java
@@ -4,7 +4,12 @@ public class Exception {
     public static void main(String[] args) {
         int[] arr = new int[]{0,1,2,3,4,5};
         System.out.println("Enter the index of the array to print its content");
-        Scanner index = new Scanner(System.in);
-        System.out.println(arr[index.nextInt()]);
+        try {
+            Scanner index = new Scanner(System.in);
+            System.out.println(arr[index.nextInt()]);
+        }
+        catch (ArrayIndexOutOfBoundsException e1) {
+            System.out.println("Problem with code detected");
+        }
     }
 }
